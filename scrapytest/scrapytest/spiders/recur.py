19d6b9fb587a1d0629d0d8e2466ebbfefc616f78
Elitack <lzg960702@163.com>
2016-12-04 16:08:43 +0800

    Adjust the judgement of the CallLog by using the img tag

--- scrapytest/scrapytest/spiders/recur.py
+++ scrapytest/scrapytest/spiders/recur.py
@@ -17,21 +17,27 @@ class QuotesSpider(scrapy.Spider):
         sys.setdefaultencoding( "utf-8" )
         indexCall = 0
         indexMes = 0
-        callLog = response.selector.xpath("//a")
+        callLog = response.selector.xpath("//td")
         for li in callLog:
-            if li.xpath("./text()").extract()[0] != u' \u901a\u8bdd\u8bb0\u5f55':
+            tmp = li.xpath("./span/img").extract()
+            if (not(tmp) or tmp[0].find("CallRecords.ico") == -1):
                 indexCall = indexCall + 1
                 continue
             break
-        for li in callLog:
-            if li.xpath("./text()").extract()[0] != u' \u5f69\u4fe1':
-                indexMes = indexMes + 1
+        indexMes = indexCall
+        for li in callLog[indexCall:len(callLog):3]:
+            tmp = li.xpath("./span/img").extract()
+            if (tmp and tmp[0].find("CallRecords.ico") != -1):
+                indexMes = indexMes + 3
                 continue
             break
-        endNum = callLog[indexCall].extract().find('.')
-        start = int(callLog[indexCall].extract()[17:endNum])
-        endNum = callLog[indexMes].extract().find('.')
-        end = int(callLog[indexMes].extract()[17:endNum])
+        indexMes = indexMes - 3
+        startA = callLog[indexCall].xpath("./span/a").extract()
+        endA = callLog[indexMes].xpath("./span/a").extract()
+        endNum = startA[0].find('.')
+        start = int(startA[0][17:endNum])
+        endNum = endA[0].find('.')
+        end = int(endA[0][17:endNum])
         for i in range(start, end+1):
             yield scrapy.Request("file://127.0.0.1/home/jack/Desktop/file2/Contents" + str(i) + ".html", callback = self.parseCallLog)
 
@@ -46,8 +52,11 @@ class QuotesSpider(scrapy.Spider):
             if lixpath != u'\u59d3\u540d':#中文:姓名
                 continue
             tdlist = li.xpath(".//td/text()").extract()
+
             while index < len(tdlist):
                 f = file("real.csv", "a+")
+                print "%d \n" %len(tdlist)
+                print "%d \n" %index
                 f.write(tdlist[index] + ',' + tdlist[index+1] + ',' + tdlist[index+2] + ',')
                 f.write(tdlist[index+4] + ',' + tdlist[index+6] + ',' + tdlist[index+8] + ',')
                 f.write(tdlist[index+10] + ',' + tdlist[index+12] + '\n')
